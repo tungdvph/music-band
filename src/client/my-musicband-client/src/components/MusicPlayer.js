49779c32c8a064b0e7c8de8a21bc33e82095047b
Van Tung <hoclaptrinhdvt1997@gmail.com>
2025-03-04 17:44:26 +0700

    css cho MuscicPlayer

--- src/client/my-musicband-client/src/components/MusicPlayer.js
+++ src/client/my-musicband-client/src/components/MusicPlayer.js
@@ -1,5 +1,6 @@
-// src/components/MusicPlayer.js
+//src/components/MusicPlayer.js
 import React, { useState, useEffect } from 'react';
+import './MusicPlayer.css'; // Import file CSS
 
 function MusicPlayer() {
     const [songs, setSongs] = useState([]);
@@ -34,15 +35,17 @@ function MusicPlayer() {
     }
 
     return (
-        <div>
+        <div className="music-player-container">
             <h1>Music Player</h1>
-            <ul>
+            <ul className="song-list">
                 {songs.map((song) => (
-                    <li key={song._id}>
-                        <h2>{song.title}</h2>
-                        <p>Artist: {song.artist}</p>
-                        <img src={song.imageUrl} alt={song.title} style={{ maxWidth: '200px' }} />
-                        <audio controls>
+                    <li key={song._id} className="song-item">
+                        <img src={song.imageUrl} alt={song.title} className="song-image" />
+                        <div className="song-info">
+                            <h2>{song.title}</h2>
+                            <p>Artist: {song.artist}</p>
+                        </div>
+                        <audio controls className="audio-player">
                             <source src={song.audioUrl} type="audio/mpeg" />
                             Your browser does not support the audio element.
                         </audio>
